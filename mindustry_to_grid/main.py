8f33ef7fb8c1f8a374f765da58dd9f6fa5643d82
RysKowalski <ryskowalski0@gmail.com>
2026-04-27 10:02:55 +0200

    s

--- mindustry_to_grid/main.py
+++ mindustry_to_grid/main.py
@@ -1,13 +1,31 @@
+from typing import Callable
 from flood_types import Cell, CellType
 from .parse import parse_save, SaveData
 
 
+def _nothingCell_factory() -> Cell:
+    return Cell(CellType.NOTHING, 0.0)
+
+
+IDMAP: dict[int, Callable[[], Cell]] = {33: _nothingCell_factory}
+
+
 def get_grid(path: str) -> list[list[Cell]]:
     parsed: SaveData = parse_save(path)
-    grid: list[list[Cell]] = _create_grid(parsed.map.width, parsed.map.height)
+    cells: list[Cell] = []
+    for tile in parsed.map.terrain:
+        cells.append(IDMAP.get(tile[2], _nothingCell_factory)())
 
+    return _create_grid(parsed.map.width, parsed.map.height, cells)
+
+
+def _create_grid(width: int, height: int, cells: list[Cell]) -> list[list[Cell]]:
+    grid: list[list[Cell]] = []
+    cellI: int = 0
+    for _ in range(height):
+        row: list[Cell] = []
+        for _ in range(width):
+            row.append(cells[cellI])
+            cellI += 1
+        grid.append(row)
     return grid
-
-
-def _create_grid(width: int, height: int) -> list[list[Cell]]:
-    return [[Cell(CellType.NOTHING, 0.0) for _ in range(height)] for _ in range(width)]
